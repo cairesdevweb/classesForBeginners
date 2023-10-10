53a1e150d32a8f4e05c378678c8108346bc8e8e8
dev-erickydias <erickyhenriquesd@gmail.com>
2023-10-10 02:25:47 +0100

    modificaçoes em aulas

---
+++ Ericky/javaScriptSprint/esplicações/index.js
@@ -0,0 +1,83 @@
+// console.log("logou")
+
+const title = document.querySelector("#title")
+var r = "dias"
+
+const g = 3
+let h = 35
+let i = 3
+var b = 10
+
+function aparecerAlerta(cor, comida) {
+  console.log(cor)
+  console.log(comida)
+}
+
+const minhaFuncao = (cor, comida) => {
+  console.log(cor)
+  console.log(comida)
+}
+
+minhaFuncao (preto, rosa)
+
+
+title.addEventListener("click", (preto, lazanha) => {
+  console.log(cor)
+  console.log(comida)
+})
+let nome = 'André'
+
+//console.log(r) 
+
+
+//console.log(b) 
+//b = 2
+console.log(b) 
+
+
+
+// Declarar uma variável com o seu nome
+let name = "Ericky"
+
+
+// Declarar uma variável com a sua idade
+const idade = 24
+
+// Declarar uma variável com a sua comida
+let comidaFavorita = "feijoada"
+console.log(comidaFavorita) 
+// cor favorita e não atribuir valor
+let corFavorita 
+console.log(corFavorita) 
+// Atribuir valor a sua cor favorita
+corFavorita = "preto"
+console.log(corFavorita) 
+
+// Declarar 5 variáveis diferentes sem valores
+let mala 
+let tenis
+let unha
+let dom
+let teclado
+
+console.log("o senhor " + name + " tem a " + idade + " anos" ) 
+console.log(`o senhor ${name} tem a ${idade} anos` )
+
+// tipos de variaveis
+// string
+let primeira= "Palavra1"
+let segunda = 3
+console.log(typeof(primeira)) 
+console.log(typeof(segunda)) 
+
+console.log(primeira + segunda) 
+
+ const frutas = ["morango", "maçã", "Banana"]
+ console.log(typeof(frutas))
+
+ let t = 0;
+ while (t < 5) {
+     console.log(t); // Isso imprimirá os números de 0 a 4
+     t++;
+     console.log(t);
+ }
